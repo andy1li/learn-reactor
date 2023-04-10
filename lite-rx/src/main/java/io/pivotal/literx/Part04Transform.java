5bd1824addb3d88db21d763648de85e3b9ec27e4
Andy Li <1450947+andy1li@users.noreply.github.com>
2023-04-11 05:17:49 +0800

    Finish lite-rx Part04Transform

--- lite-rx/src/main/java/io/pivotal/literx/Part04Transform.java
+++ lite-rx/src/main/java/io/pivotal/literx/Part04Transform.java
@@ -13,23 +13,31 @@ public class Part04Transform {
 
 //========================================================================================
 
-	// TODO Capitalize the user username, firstname and lastname
+	User capitalizeUser(User u) {
+		return new User(
+			u.getUsername().toUpperCase(),
+			u.getFirstname().toUpperCase(),
+			u.getLastname().toUpperCase()
+		);
+	}
+
+	// Capitalize the user username, firstname and lastname
 	Mono<User> capitalizeOne(Mono<User> mono) {
-		return null;
+		return mono.map(this::capitalizeUser);
 	}
 
 //========================================================================================
 
-	// TODO Capitalize the users username, firstName and lastName
+	// Capitalize the users username, firstName and lastName
 	Flux<User> capitalizeMany(Flux<User> flux) {
-		return null;
+		return flux.map(this::capitalizeUser);
 	}
 
 //========================================================================================
 
-	// TODO Capitalize the users username, firstName and lastName using #asyncCapitalizeUser
+	// Capitalize the users username, firstName and lastName using #asyncCapitalizeUser
 	Flux<User> asyncCapitalizeMany(Flux<User> flux) {
-		return null;
+		return flux.flatMap(this::asyncCapitalizeUser);
 	}
 
 	Mono<User> asyncCapitalizeUser(User u) {
